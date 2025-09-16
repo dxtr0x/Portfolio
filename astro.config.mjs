24ba5820f21658342a63f608a138b896c857dcd8
dxtr0x <dextr_x.io@proton.me>
2025-09-16 23:23:26 +0100

    add contact section

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 import netlify from '@astrojs/netlify';
 
+
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: []
 });
